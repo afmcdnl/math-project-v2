2126db7db46ec33599daa89ac927ae296aec38b5
Andrew <afmcdnl@gmail.com>
2021-10-21 20:21:55 -0400

    Added start function and checking guess and answer

--- main.ts
+++ main.ts
@@ -1,19 +1,31 @@
+function Start () {
+    Numb1 = 0
+    Numb2 = 0
+    state = 0
+    answer = 0
+    _symbol = 0
+    guess = 0
+}
 input.onButtonPressed(Button.A, function () {
     if (state == 0) {
     	
     } else if (state == 2) {
         guess += -1
-    } else {
-    	
     }
 })
 input.onButtonPressed(Button.AB, function () {
     if (state == 0) {
         state = 1
     } else if (state == 2) {
-    	
-    } else {
-    	
+        state = 3
+    } else if (state == 3) {
+        if (answer == guess) {
+            basic.showIcon(IconNames.Yes)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+        basic.pause(500)
+        Start()
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -21,17 +33,15 @@ input.onButtonPressed(Button.B, function () {
     	
     } else if (state == 2) {
         guess += 1
-    } else {
-    	
     }
 })
-let state = 0
-let Numb1 = 0
-let Numb2 = 0
-state = 0
-let answer = 0
-let _symbol = 0
 let guess = 0
+let _symbol = 0
+let answer = 0
+let state = 0
+let Numb2 = 0
+let Numb1 = 0
+Start()
 basic.forever(function () {
     if (state == 0) {
     	
@@ -53,7 +63,5 @@ basic.forever(function () {
         state = 2
     } else if (state == 2) {
         basic.showNumber(guess)
-    } else {
-    	
     }
 })
